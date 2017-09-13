a721fe54ba3d4eef6cccea9f8524a2793696dcc5
Clément Thomas <clement@cotyl.net>
2017-09-13 09:57:36 +0200

    Ajout du module template

---
+++ module_template/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Digisuite module template',
+    'version': '10.0',
+    'category': 'Other',
+    'author': "Digisuite"
+    'website': 'http://www.digisuite.fr',
+    'license': 'AGPL-3',
+    'depends': [''],
+    'data': [
+    ],
+    'installable': True,
+}
